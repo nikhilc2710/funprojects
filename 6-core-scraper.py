582c33cc8eb0d025331bb362c2e06954dfe7d2f5
nikhilc2710 <nikhilchauhan839@gmail.com>
2020-12-11 20:52:02 +0530

    :blue_heart: 6-Core-Scrapper done :blue_heart::tada:

--- 6-core-scraper.py
+++ 6-core-scraper.py
@@ -2,24 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 from itertools import chain
 from multiprocessing import Pool
-
+import sys
 def mailfinder(url):
     r=requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
     mails = soup.findAll("div", {"class": "city_tab"})
     hotel_ref=[i.find('a',href=True)['href']for i in mails  if "View Email ID" in i.text ]
     return hotel_ref
+    
 def mailextractor(ref):
     r=requests.get(ref)
     soup = BeautifulSoup(r.text, 'lxml')
     mails = soup.findAll("td", {"class": "table_space_td_right1"})
     return  mails[-1].text.split(',')
 
-    
-
-l=["somesecret"]+[f"some secret"]
-
 if __name__ == "__main__":
+    _,url,start,end=sys.argv
+    l=[url+f"/pag={i}/" for i in range(int(start),int(end)+1)]
     with Pool(12) as p:
         mp=p.map(mailfinder, l)
     _hotel_ref=list(chain(*mp))
