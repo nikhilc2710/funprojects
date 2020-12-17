8669ec88dc15146c5eceba0aa549ba4a941f0873
nikhilc2710 <nikhilchauhan839@gmail.com>
2020-12-17 09:17:30 +0530

    Modified BFS_

--- Algoscripts/bfs_.py
+++ Algoscripts/bfs_.py
@@ -3,21 +3,21 @@ def BFS(graph,node):
     visited=set()
     q=deque()
     q.append(node)
+    visited.add(node)
     while q:
         n=q.popleft()
-        if n not in visited:
-            visited.add(n)
-            print(n,end=" ")
-
+        print(n,end=" ")
         for i in graph[n]:
-            q.append(i)
+            if i not in visited:
+                visited.add(i)
+                q.append(i)
 
 graph={
     'A' : ['B','C'],
   'B' : ['D', 'E'],
   'C' : ['F'],
-  'D' : [],
+  'D' : ['A'],
   'E' : ['F'],
   'F' : []
 }
-BFS(graph,'F')
+BFS(graph,'B')
