8802cb5b57f5a994f9e25236c07281dd352377b1
nikhilc2710 <nikhilchauhan839@gmail.com>
2020-12-27 20:23:05 +0530

    Fask and FastAPI sever post template :blue_heart: :tada:

---
+++ send_post_to_flask_fastapi_server.py
@@ -0,0 +1,34 @@
+# import requests
+# import json
+#FCM Token
+# payload = {
+#  "notification": {
+#  "title": "Hey there", 
+#  "body": "Mobile check"
+#  },
+#  "to" :"el-8P3oPU_ahSp6c0kpfou:APA912HvqW8gVHoBiHaeK3er4c9s"
+# }
+# head={ 'content-type': "application/json",
+#     'authorization': "key=AAA-jnE:APA91bFHken2X1z_kHAwXjeMRvPftRRAXFT9nBWS4wzSHlOkq3h7ayaldecX6SWl4GD7Y3qZ837zhwn9AiQF",
+#     'cache-control': "no-cache",
+#     'postman-token': "1e7e57b164"
+# }
+# res=requests.post('https://fcm.googleapis.com/fcm/send',data=json.dumps(payload),headers=head)
+# print(res.text)
+#Flask Server
+# import requests
+import json
+# payload = {
+# "list":['url1','url2']}
+# print('hello')
+# res=requests.post('hosted or local host',data=json.dumps(payload))
+# print(res.text)
+
+#FAST API SERVER
+
+import requests
+import json
+payload = {
+"url":'url'}
+res=requests.post('hosted or localhost',data=json.dumps(payload))
+x=res.json()
