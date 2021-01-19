25eb58dd91b039c0de85d00c9222c4faba07b77a
nikhilc2710 <nikhilchauhan839@gmail.com>
2021-01-19 21:44:17 +0530

    VERY IMPORT BATCH REPLACE :blue_heart:

--- Algoscripts/batchReplace.py
+++ Algoscripts/batchReplace.py
@@ -10,3 +10,11 @@ for i in range(1,len(s),3):
 for i in range(2,len(s),3):
     ans[i]=s[i].upper()
 print(' '.join(ans))
+
+############Even more optimized #####################
+from itertools import chain
+vowels={'a':'%','e':'%','i':'%','o':'%','u':'%'}
+constants={chr(i).lower():'#' for i in range(65,91) if chr(i).lower() not in vowels}
+s=input().split()
+z=' '.join((chain(*zip(*[[i.translate(str.maketrans(vowels)) for i in s[0:len(s):3]],[i.translate(str.maketrans(constants)) for i in  s[1:len(s):3]],[i.upper() for i in s[2:len(s):3]]]))))
+print(z)
