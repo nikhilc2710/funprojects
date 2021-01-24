37eee7ef69d13ed9a6041ea2e71f9ffc3631ef9e
nikhilc2710 <nikhilchauhan839@gmail.com>
2021-01-24 22:16:39 +0530

    Palindrome using Recursion

--- Algoscripts/bitoperations.py
+++ Algoscripts/bitoperations.py
@@ -1,20 +1,4 @@
-# Count number of ones in very long binary represnation
-# https://leetcode.com/problems/number-of-1-bits/
-# 11111111111111111111111111111101 carzy long numbers
-# Logic:
-# like int n%10 number is binary so we use n%2 if yes increament ans 
-# and instead of n//10 we do n//2 to get rid of last digit
-    def hammingWeight(self, n: str) -> int:
-        ans=0
-        while n:
-            if n%2: ans += 1
-            n//=2
-        return ans
-
-# Add two numbers without any arithemetic operators #IMP# 
-        MASK=0xffffffff #to round up reslut and loop upto 32 bit
-        while b & MASK:
-            carray=a & b
-            a=a^b
-            b=carray<<1
-        return (a & MASK) if b>0 else a
+import re
+cond = re.compile('#<.*/>')
+output = re.sub(cond, "#", "#<a href=\"stuff1\">stuff1</a>")
+print(output)
---
+++ Algoscripts/plaiusingRecursion.py
@@ -0,0 +1,10 @@
+S="raceacar"
+
+def ispali(s):
+    if len(s)<1:
+        return True
+    elif s[0]==s[-1]:
+        return ispali(s[1:-1])
+    else:
+        return False
+print(ispali(S))
