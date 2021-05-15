64744fe0b31dd88fe7ac32ee02c37abb862b62c2
nikhilc2710 <nikhilchauhan839@gmail.com>
2021-05-16 03:01:27 +0530

    github to drive pipeline :blue_heart: :tada: :fire:

---
+++ Google.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('drive', 'v3', credentials=creds)
+    print("done")
+    return service
+
+    # Call the Drive v3 API
+    # results = service.files().list(
+    #     pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    # items = results.get('files', [])
+
+    # if not items:
+    #     print('No files found.')
+    # else:
+    #     print('Files:')
+    #     for item in items:
+    #         print(item)
+    file_metadata = {'name': 'serverbackup_upto2021_05_16_01_34_18.zip'}
+    media = MediaFileUpload("serverbackup_upto2021_05_16_01_34_18.zip", mimetype='application/zip')
+    file = service.files().create(body=file_metadata,
+                                    media_body=media,
+                                    fields='id').execute()
+    print ('File ID: %s' % file.get('id'))
+# if __name__ == '__main__':
+#     main()
